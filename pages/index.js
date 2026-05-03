7caa2a01437cb67145675b7fc4c99a56da10dfbd
Jext1 <126114519+Jext1@users.noreply.github.com>
2026-05-03 11:38:56 +0100

    Refactor login state variables and input handlers

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [u, setU] = useState("");
+  const [p, setP] = useState("");
 
   async function login() {
     const res = await fetch("/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password })
+      body: JSON.stringify({ username: u, password: p })
     });
 
     const data = await res.json();
@@ -17,7 +17,7 @@ export default function Home() {
       localStorage.setItem("token", data.token);
       window.location.href = "/dashboard";
     } else {
-      alert("Login failed");
+      alert("Wrong login");
     }
   }
 
@@ -25,17 +25,10 @@ export default function Home() {
     <div style={{ textAlign: "center", marginTop: 100 }}>
       <h1>Bouncin Llanelli Admin</h1>
 
-      <input
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
-      />
+      <input placeholder="Username" onChange={e => setU(e.target.value)} />
       <br />
 
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <input type="password" placeholder="Password" onChange={e => setP(e.target.value)} />
       <br />
 
       <button onClick={login}>Login</button>
