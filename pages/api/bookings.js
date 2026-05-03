0f690d28af5c061ce1106b7806fb8009e09cc6f4
Jext1 <126114519+Jext1@users.noreply.github.com>
2026-05-03 10:43:16 +0100

    Change API key in bookings.js
    Updated the API key for Google Calendar integration.

--- pages/api/bookings.js
+++ pages/api/bookings.js
@@ -2,7 +2,7 @@ import { parseBooking } from "../../lib/parser";
 
 export default async function handler(req, res) {
   const calendarId = "YOUR_CALENDAR_ID";
-  const apiKey = "YOUR_API_KEY";
+  const apiKey = "AIzaSyCQiICbQrKFRbr9c80_-7XNlfwhScpqYO4";
 
   const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}`;
 
